f537e84dafd47769e0e8ff21bdde66d9aac91051
ahmet <ahmet@DESKTOP-HSYN>
2022-12-08 00:56:46 +0300

    Created ModelBase.

---
+++ Models/ModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ModelBase
+    {
+        [Key]
+        public Guid Id { get; set; }= Guid.NewGuid();
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateModified { get; set; } = DateTime.Now;
+
+        public bool IsDeleted { get; set; }    = false;
+        public bool IsActive { get; set; } = true; 
+
+
+
+    }
+}
