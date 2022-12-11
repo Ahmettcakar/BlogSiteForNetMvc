21b226ba45701250708e71946db4e34aab9ee112
ahmet <ahmet@DESKTOP-HSYN>
2022-12-11 23:57:12 +0300

    educationcity added education class

---
+++ Data/Migrations/20221211205625_AddedEducationCityinEducationClass.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEducationCityinEducationClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EducationCity",
+                table: "Educations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EducationCity",
+                table: "Educations");
+        }
+    }
+}
