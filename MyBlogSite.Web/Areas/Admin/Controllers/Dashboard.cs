3419dec64d07a1ccd650a2b6ab597aab9b3b4ecf
ahmet <ahmet@DESKTOP-HSYN>
2022-12-13 01:10:48 +0300

    Admin Area and Dashboard Created

---
+++ MyBlogSite.Web/Areas/Admin/Controllers/Dashboard.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogSite.Web.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class Dashboard : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
