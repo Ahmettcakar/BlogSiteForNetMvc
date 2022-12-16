e7ad46f411cca20f84182c2e49aa95e024cdcb2e
ahmet <ahmet@DESKTOP-HSYN>
2022-12-16 03:32:50 +0300

    Editable plugin add Experience tables

--- MyBlogSite.Web/Areas/Admin/Controllers/Experience.cs
+++ MyBlogSite.Web/Areas/Admin/Controllers/Experience.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyBlogSite.Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
 	public class Experience : Controller
 	{
-		public IActionResult Index()
+		private readonly ApplicationDbContext _db;
+
+        public Experience(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
 		{
 			return View();
 		}
-	}
+        public IActionResult GetAllExperience()
+        {
+            return Json(_db.experiences.Where(e => e.IsActive == true && e.IsDeleted == false).OrderByDescending(e => e.EndDate).ToList());
+        }
+
+    }
 }
